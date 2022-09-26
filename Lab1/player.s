161a5bc5cf4d770cfa5efde9efe3335a542f9603
cyclexit <cyclexit@outlook.com>
2022-09-26 13:22:35 -0400

    Correct the branch in assembly

--- Lab1/player.s
+++ Lab1/player.s
@@ -18,12 +18,12 @@ player_check_move_s FUNCTION	; char player_check_move_s(player_t *player, int ro
 	MOV R8, #1 ; R8 = 1
 	LSL R8, R8, R7 ; R8 = R8 << R7
 	AND R8, R6 ; R8 = R8 & R6
-	TST R8, #1
-	CBNZ R0, RET1
-	MOV	R0, #0					; 
+	CMP R8, #0
+	BNE RET0
+	MOV	R0, #1					; 
 	BX	LR						; return
-RET1
-	MOV R0, #1
+RET0
+	MOV R0, #0
 	BX	LR						; return
 
   ENDFUNC
@@ -36,7 +36,7 @@ player_make_move_s FUNCTION	; char player_make_move_s(player_t *player, int row,
 	; r1 row
 	; r2 col
 
-	PUSH {R6, R7, R8, R9}
+	PUSH {R6, R7, R8, R9, LR}
 	LDRH R6, [R0] ; R6 = player->state
 	LSL R7, R1, #2 ; R7 = row << 2
 	ADD R7, R7, R2 ; R7 = R7 + col
@@ -44,14 +44,14 @@ player_make_move_s FUNCTION	; char player_make_move_s(player_t *player, int row,
 	LSL R8, R8, R7 ; R8 = R8 << R7
 	MOV R9, R8
 	AND R8, R6 ; R8 = R8 & R6
-	TST R8, #1
-	CBNZ R0, STORE
-	MOV	R0, #0					;
-	BX	LR						; return
-STORE 
+	CMP R8, #0
+	BNE RET0_1
 	ORR R9, R6 ; R9 |= R6
 	STRH R9, [R0]
-	MOV R0, #1
+	MOV	R0, #1					;
+	BX	LR						; return
+RET0_1
+	MOV R0, #0
 	BX	LR						; return
 
   ENDFUNC
