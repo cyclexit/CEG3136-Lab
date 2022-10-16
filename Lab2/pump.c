b8a5bde33cf5f51e8667953533ebafa4c3be8a1d
cyclexit <cyclexit@outlook.com>
2022-10-16 14:24:30 -0400

    Implement the pump_update_state function

--- Lab2/pump.c
+++ Lab2/pump.c
@@ -11,5 +11,29 @@ void pump_init(pump_t *pump) {
 pump_state_e pump_update_state(pump_t *pump, float tank_height) {
   // this is just a stub!
   // implement the state machine here
-  return OFF;
+  switch (pump->state) {
+    case OFF:
+      if (tank_height < ALMOST_EMPTY) {
+        pump->flow = 1.0;
+        pump->state = ON_HIGH;
+      }
+      break;
+    case ON_HIGH:
+      if (tank_height > ALMOST_FULL) {
+        pump->flow = 0.5;
+        pump->state = ON_LOW;
+      }
+      break;
+    case ON_LOW:
+      if (tank_height > TANK_FULL) {
+        pump->flow = 0.0;
+        pump->state = OFF;
+      }
+      break;
+    default:
+      printf("Error: Unknown pump state!\n");
+      break;
+  }
+
+  return pump->state;
 }
